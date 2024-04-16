ab8f6f28c94b13c40973d5edd8e6b18a38a0ce75
Ray Ruenheck <rayruenheck@gmail.com>
2024-04-16 14:42:32 -0400

    failing at mongodb connection

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from flask_cors import CORS
 from flask_pymongo import PyMongo
 from app.auth.users import users_bp
-from flask_jwt_extended import JWTManager
+
 
 load_dotenv()
 
@@ -12,11 +12,10 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 
-app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
 
 CORS(app, supports_credentials=True)
 
-jwt = JWTManager(app)
+
 mongo = PyMongo(app)
 
 app.register_blueprint(users_bp)
